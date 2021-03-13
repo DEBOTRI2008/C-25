47b07ec4b67ff1ff1fe25a0c4850c531308ceceb
DEBOTRI2008 <75121884+DEBOTRI2008@users.noreply.github.com>
2021-03-13 12:55:23 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,26 @@
+class Pig extends BaseClass {
+    constructor(x, y) {
+    
+      super(x,y,50,50);
+      this.image=loadImage ("sprites/enemy.png")
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    
+    
+    
+
+    
+    
+    }
+      }
+  
